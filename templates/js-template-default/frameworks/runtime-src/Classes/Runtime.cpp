ed84edb889f3d193a54f35f73287fc660ca4b6e2
chuanweizhang2013 <chuanweizhang2013@gmail.com>
2014-03-04 17:04:34 +0800

    write file w -->wb

--- templates/js-template-default/frameworks/runtime-src/Classes/Runtime.cpp
+++ templates/js-template-default/frameworks/runtime-src/Classes/Runtime.cpp
@@ -553,7 +553,7 @@ bool FileServer::recv_file(int fd)
 	sprintf(fullfilename, "%s", file.c_str());
 	cocos2d::log("recv fullfilename = %s",fullfilename);
     CreateDir(file.substr(0,file.find_last_of("/")).c_str());
-	FILE *fp =fopen(fullfilename, "w");
+	FILE *fp =fopen(fullfilename, "wb");
 	
 	int length =0;
 	while ((length=read(fd, fullfilename, sizeof(fullfilename))) > 0) {
