ce34c2be90b6c6cbe7b8f1e8d5a3b72ba9f24819
joshuastray <joshua.astray.14@gmail.com>
2014-04-24 17:36:29 +0800

    Add application foreground and background status JSB support.

--- templates/js-template-default/frameworks/runtime-src/Classes/AppDelegate.cpp
+++ templates/js-template-default/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -70,17 +70,19 @@ bool AppDelegate::applicationDidFinishLaunching()
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
 void AppDelegate::applicationDidEnterBackground()
 {
-    Director::getInstance()->stopAnimation();
+    auto director = Director::getInstance();
+    director->stopAnimation();
+    director->getEventDispatcher()->dispatchCustomEvent("game_on_hide");
     SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
-    SimpleAudioEngine::getInstance()->pauseAllEffects();
-    Director::getInstance()->getEventDispatcher()->dispatchCustomEvent("game_on_hide");
+    SimpleAudioEngine::getInstance()->pauseAllEffects();    
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground()
 {
-    Director::getInstance()->startAnimation();
+    auto director = Director::getInstance();
+    director->startAnimation();
+    director->getEventDispatcher()->dispatchCustomEvent("game_on_show");
     SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
     SimpleAudioEngine::getInstance()->resumeAllEffects();
-    Director::getInstance()->getEventDispatcher()->dispatchCustomEvent("game_on_show");
 }
