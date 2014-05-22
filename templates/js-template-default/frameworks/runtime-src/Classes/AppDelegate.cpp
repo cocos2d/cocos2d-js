e1d0b9b1e523903bcb0a947659541d1f43a6f83b
pandamicro <lphuabin@gmail.com>
2014-05-22 21:58:23 +0800

    Fixed #5022: Add spine, websocket, socket to template by default

--- templates/js-template-default/frameworks/runtime-src/Classes/AppDelegate.cpp
+++ templates/js-template-default/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -1,19 +1,23 @@
 #include "AppDelegate.h"
 
-#include "cocosbuilder/js_bindings_ccbreader.h"
 #include "SimpleAudioEngine.h"
 #include "jsb_cocos2dx_auto.hpp"
 #include "jsb_cocos2dx_ui_auto.hpp"
 #include "jsb_cocos2dx_studio_auto.hpp"
-#include "jsb_cocos2dx_extension_auto.hpp"
 #include "jsb_cocos2dx_builder_auto.hpp"
+#include "jsb_cocos2dx_spine_auto.hpp"
+#include "jsb_cocos2dx_extension_auto.hpp"
 #include "ui/jsb_cocos2dx_ui_manual.h"
-#include "extension/jsb_cocos2dx_extension_manual.h"
 #include "cocostudio/jsb_cocos2dx_studio_manual.h"
+#include "cocosbuilder/js_bindings_ccbreader.h"
+#include "spine/jsb_cocos2dx_spine_manual.h"
+#include "extension/jsb_cocos2dx_extension_manual.h"
 #include "localstorage/js_bindings_system_registration.h"
 #include "chipmunk/js_bindings_chipmunk_registration.h"
 #include "jsb_opengl_registration.h"
 #include "network/XMLHTTPRequest.h"
+#include "network/jsb_websocket.h"
+#include "network/jsb_socketio.h"
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
 #include "platform/android/CCJavascriptJavaBridge.h"
 #endif
@@ -50,22 +54,30 @@ bool AppDelegate::applicationDidFinishLaunching()
     sc->addRegisterCallback(register_all_cocos2dx_extension);
     sc->addRegisterCallback(register_cocos2dx_js_extensions);
     sc->addRegisterCallback(register_all_cocos2dx_extension_manual);
+    sc->addRegisterCallback(jsb_register_chipmunk);
+    sc->addRegisterCallback(jsb_register_system);
+    sc->addRegisterCallback(JSB_register_opengl);
+    
     sc->addRegisterCallback(register_all_cocos2dx_builder);
     sc->addRegisterCallback(register_CCBuilderReader);
+    
 	sc->addRegisterCallback(register_all_cocos2dx_ui);
 	sc->addRegisterCallback(register_all_cocos2dx_ui_manual);
 	sc->addRegisterCallback(register_all_cocos2dx_studio);
 	sc->addRegisterCallback(register_all_cocos2dx_studio_manual);
-    sc->addRegisterCallback(jsb_register_system);
-    sc->addRegisterCallback(JSB_register_opengl);
-    sc->addRegisterCallback(jsb_register_chipmunk);
+    
+    sc->addRegisterCallback(register_all_cocos2dx_spine);
+    sc->addRegisterCallback(register_all_cocos2dx_spine_manual);
+    
     sc->addRegisterCallback(MinXmlHttpRequest::_js_register);
+    sc->addRegisterCallback(register_jsb_websocket);
+	sc->addRegisterCallback(register_jsb_socketio);
+    
     #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
     sc->addRegisterCallback(JavascriptJavaBridge::_js_register);
     #endif
     sc->start();
     
-
     ScriptEngineProtocol *engine = ScriptingCore::getInstance();
 	ScriptEngineManager::getInstance()->setScriptEngine(engine);
 	ScriptingCore::getInstance()->runScript("main.js");
