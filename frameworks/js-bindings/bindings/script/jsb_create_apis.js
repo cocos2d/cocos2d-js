f77f98e8f8c24ac265b04bf7a6607c1261036918
joshuastray <joshua.astray.14@gmail.com>
2014-07-16 09:59:35 +0800

    issue #5685: fix labelatlas _ctor bug

--- frameworks/js-bindings/bindings/script/jsb_create_apis.js
+++ frameworks/js-bindings/bindings/script/jsb_create_apis.js
@@ -766,7 +766,7 @@ cc.LabelAtlas.prototype._ctor = function(strText, charMapFile, itemWidth, itemHe
 	if (charMapFile) {
 		itemWidth = itemWidth || 0;
 		itemHeight = itemHeight || 0;
-		startCharMap = startCharMap || "";
+		startCharMap = startCharMap.charCodeAt(0) || 0;
 		cc.LabelAtlas.prototype.initWithString.call(this, strText, charMapFile, itemWidth, itemHeight, startCharMap);
 	}
 };
