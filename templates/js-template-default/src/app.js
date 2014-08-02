07f5660c587709f52c9c84ed5af5cb00a3374056
pandamicro <lphuabin@gmail.com>
2014-08-02 14:51:09 +0800

    Fixed #5760: Improve templates HelloWorld project

--- templates/js-template-default/src/app.js
+++ templates/js-template-default/src/app.js
@@ -9,11 +9,11 @@ var HelloWorldLayer = cc.Layer.extend({
         /////////////////////////////
         // 2. add a menu item with "X" image, which is clicked to quit the program
         //    you may modify it.
-        // ask director the window size
-        var size = cc.director.getWinSize();
+        // ask the window size
+        var size = cc.winSize;
 
         // add a "close" icon to exit the progress. it's an autorelease object
-        var closeItem = cc.MenuItemImage.create(
+        var closeItem = new cc.MenuItemImage(
             res.CloseNormal_png,
             res.CloseSelected_png,
             function () {
@@ -26,7 +26,7 @@ var HelloWorldLayer = cc.Layer.extend({
             anchorY: 0.5
         });
 
-        var menu = cc.Menu.create(closeItem);
+        var menu = new cc.Menu(closeItem);
         menu.x = 0;
         menu.y = 0;
         this.addChild(menu, 1);
@@ -35,7 +35,7 @@ var HelloWorldLayer = cc.Layer.extend({
         // 3. add your codes below...
         // add a label shows "Hello World"
         // create and initialize a label
-        var helloLabel = cc.LabelTTF.create("Hello World", "Arial", 38);
+        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
         // position the label on the center of the screen
         helloLabel.x = size.width / 2;
         helloLabel.y = 0;
@@ -43,7 +43,7 @@ var HelloWorldLayer = cc.Layer.extend({
         this.addChild(helloLabel, 5);
 
         // add "HelloWorld" splash screen"
-        this.sprite = cc.Sprite.create(res.HelloWorld_png);
+        this.sprite = new cc.Sprite(res.HelloWorld_png);
         this.sprite.attr({
             x: size.width / 2,
             y: size.height / 2,
@@ -52,11 +52,18 @@ var HelloWorldLayer = cc.Layer.extend({
         });
         this.addChild(this.sprite, 0);
 
-        var rotateToA = cc.RotateTo.create(2, 0);
-        var scaleToA = cc.ScaleTo.create(2, 1, 1);
-
-        this.sprite.runAction(cc.Sequence.create(rotateToA, scaleToA));
-        helloLabel.runAction(cc.Spawn.create(cc.MoveBy.create(2.5, cc.p(0, size.height - 40)),cc.TintTo.create(2.5,255,125,0)));
+        this.sprite.runAction(
+            cc.sequence(
+                cc.rotateTo(2, 0),
+                cc.scaleTo(2, 1, 1)
+            )
+        );
+        helloLabel.runAction(
+            cc.spawn(
+                cc.moveBy(2.5, cc.p(0, size.height - 40)),
+                cc.tintTo(2.5,255,125,0)
+            )
+        );
         return true;
     }
 });
--- templates/js-template-runtime/main.js
+++ templates/js-template-runtime/main.js
@@ -1,7 +1,7 @@
 cc.game.onStart = function(){
     cc.view.adjustViewPort(true);
-    cc.view.setDesignResolutionSize(800, 450, cc.ResolutionPolicy.EXACT_FIT);
-	cc.view.resizeWithBrowserSize(true);
+    cc.view.setDesignResolutionSize(800, 450, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.resizeWithBrowserSize(true);
     //load resources
     cc.LoaderScene.preload(g_resources, function () {
         cc.director.runScene(new HelloWorldScene());
