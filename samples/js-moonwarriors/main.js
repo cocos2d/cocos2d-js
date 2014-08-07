64fc93c6ad05ee0d87a9de404ad216b14dbd3578
dingpinglv <dingping.lv@cocos2d-x.org>
2014-08-07 18:23:11 +0800

    Fixed #5781: update build.xml of MoonWarriors

--- samples/js-moonwarriors/main.js
+++ samples/js-moonwarriors/main.js
@@ -32,10 +32,9 @@ cc.game.onStart = function(){
     cc.director.setProjection(cc.Director.PROJECTION_2D);
 
     if (cc.sys.isNative) {
-        var searchPaths = jsb.fileUtils.getSearchPaths()
-        searchPaths.push('script')
-        if (cc.sys.os == cc.sys.OS_IOS || cc.sys.os == cc.sys.OS_OSX)
-        {
+        var searchPaths = jsb.fileUtils.getSearchPaths();
+        searchPaths.push('script');
+        if (cc.sys.os == cc.sys.OS_IOS || cc.sys.os == cc.sys.OS_OSX) {
             searchPaths.push("res");
             searchPaths.push("src");
         }
