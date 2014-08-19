20828d7e50cd737b5bf5adfc1ac0b5aa106047a1
joshuastray <joshua.astray.14@gmail.com>
2014-08-19 13:48:53 +0800

    Refactor #5814: pass select_toolchain_version

--- build/android-build.py
+++ build/android-build.py
@@ -9,21 +9,21 @@ from optparse import OptionParser
 
 JS_SAMPLES = ['js-tests', 'js-moonwarriors']
 def get_num_of_cpu():
-        ''' The build process can be accelerated by running multiple concurrent job processes using the -j-option.
-        '''
-        try:
-                platform = sys.platform
-                if platform == 'win32':
-                        if 'NUMBER_OF_PROCESSORS' in os.environ:
-                                return int(os.environ['NUMBER_OF_PROCESSORS'])
-                        else:
-                                return 1
-                else:
-                        from numpy.distutils import cpuinfo
-                        return cpuinfo.cpu._getNCPUs()
-        except Exception:
-                print "Can't know cpuinfo, use default 1 cpu"
+    ''' The build process can be accelerated by running multiple concurrent job processes using the -j-option.
+    '''
+    try:
+        platform = sys.platform
+        if platform == 'win32':
+            if 'NUMBER_OF_PROCESSORS' in os.environ:
+                return int(os.environ['NUMBER_OF_PROCESSORS'])
+            else:
                 return 1
+        else:
+            from numpy.distutils import cpuinfo
+            return cpuinfo.cpu._getNCPUs()
+    except Exception:
+        print "Can't know cpuinfo, use default 1 cpu"
+        return 1
 
 def check_environment_variables():
     ''' Checking the environment NDK_ROOT, which will be used for building
@@ -57,16 +57,17 @@ def select_toolchain_version():
     ndk-r9   -> use gcc4.8
     '''
 
-    ndk_root = check_environment_variables()
-    if os.path.isdir(os.path.join(ndk_root,"toolchains/arm-linux-androideabi-4.8")):
-        os.environ['NDK_TOOLCHAIN_VERSION'] = '4.8'
-        print "The Selected NDK toolchain version was 4.8 !"
-    elif os.path.isdir(os.path.join(ndk_root,"toolchains/arm-linux-androideabi-4.7")):
-        os.environ['NDK_TOOLCHAIN_VERSION'] = '4.7'
-        print "The Selected NDK toolchain version was 4.7 !"
-    else:
-        print "Couldn't find the gcc toolchain."
-        exit(1)
+    # ndk_root = check_environment_variables()
+    # if os.path.isdir(os.path.join(ndk_root,"toolchains/arm-linux-androideabi-4.8")):
+    #     os.environ['NDK_TOOLCHAIN_VERSION'] = '4.8'
+    #     print "The Selected NDK toolchain version was 4.8 !"
+    # elif os.path.isdir(os.path.join(ndk_root,"toolchains/arm-linux-androideabi-4.7")):
+    #     os.environ['NDK_TOOLCHAIN_VERSION'] = '4.7'
+    #     print "The Selected NDK toolchain version was 4.7 !"
+    # else:
+    #     print "Couldn't find the gcc toolchain."
+    #     exit(1)
+    pass
 
 def do_build(cocos_root, ndk_root, app_android_root, ndk_build_param,sdk_root,android_platform,build_mode):
 
@@ -80,7 +81,7 @@ def do_build(cocos_root, ndk_root, app_android_root, ndk_build_param,sdk_root,an
         ndk_module_path = 'NDK_MODULE_PATH=%s/..:%s:%s/external:%s/cocos' % (cocos_root, cocos_root, cocos_root, cocos_root)
 
     num_of_cpu = get_num_of_cpu()
-    if ndk_build_param == None:
+    if ndk_build_param is None:
         command = '%s -j%d -C %s %s' % (ndk_path, num_of_cpu, app_android_root, ndk_module_path)
     else:
         command = '%s -j%d -C %s %s %s' % (ndk_path, num_of_cpu, app_android_root, ndk_build_param, ndk_module_path)
