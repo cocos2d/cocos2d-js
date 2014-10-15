8df7270bb0239d9843c87ee4f741c602c6824150
jianglong0156 <jianglong0156@gmail.com>
2014-10-15 17:10:24 +0800

    issue #5977: update testcase add , make the sidebar changeed when user click right and left in the test scene

--- samples/js-tests/lib/TestCase/testcase.js
+++ samples/js-tests/lib/TestCase/testcase.js
@@ -387,11 +387,16 @@
             });
         },
 
-        changeTest: function() {
-            return sidebar._testNum;
+        changeTest: function(testChangeNum) {
+            if (sidebar._testNum == testChangeNum)
+            {
+                return testChangeNum;
+            }
+            sidebar.changeHover(testChangeNum, sidebar._sceneNum);
+            return testChangeNum;
         },
 
-        changeTestScene: function(actualTestNum, actualSceneNum){
+        changeHover:function (actualTestNum, actualSceneNum) {
             if(sidebar._sceneNum == actualSceneNum){
 
                 if(sidebar._testNum < actualTestNum){
@@ -415,6 +420,10 @@
             sidebar._testNum = actualTestNum;
 
             this._hiddenOtherHover(actualTestNum, actualSceneNum);
+        },
+
+        changeTestScene: function(actualTestNum, actualSceneNum){
+            sidebar.changeHover(actualTestNum, actualSceneNum);
 
             //The same scene
             cc.LoaderScene.preload(testNames[sidebar.diplayRecordArr[actualSceneNum]].resource || [], function () {
