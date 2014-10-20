8ab38471fc2acd598f7337c6dcd6f5bc49f64b5b
VisualSJ <itharbors@outlook.com>
2014-10-20 21:50:20 +0800

    Issue #5983: Modifying the test code

--- samples/js-tests/src/CocosStudio2.0/UIFromCsb.js
+++ samples/js-tests/src/CocosStudio2.0/UIFromCsb.js
@@ -52,19 +52,33 @@ var CocosStudio2_UIFromCsb = (function(){
             backMenu.y = 40;
             backMenu.addChild(backItem);
             this.addChild(backMenu);
+
+            this._pMenu = pMenu;
+            this._backMenu = backMenu;
         },
 
         menuCallback:function (sender) {
             var nIndex = sender.zIndex - ITEM_TAG_BASIC;
             var layer = this;
-            var node = ccs.uiReader.widgetFromProtocolBuffers(g_ccs2[nIndex]);
-            node.setScale(1.4);
-            node.x = 60;
-            layer.addChild(node);
+            var node = ccs.csLoader.createNode(g_ccs2[nIndex]);
+            var child = node._children[0];
+            child.removeFromParent(false);
+            this.addChild(child);
 
-            var back = ccui.helper.seekWidgetByName(node, "back");
+            var screenSize = cc.director.getWinSize();
+            var rootSize = child.getContentSize();
+
+            child.setPosition(cc.p((screenSize.width - rootSize.width) / 2,
+                    (screenSize.height - rootSize.height) / 2));
+
+            this._pMenu.setVisible(0);
+            this._backMenu.setVisible(0);
+
+            var back = ccui.helper.seekWidgetByName(child, "back");
             back.addTouchEventListener(function(){
-                layer.removeChild(node);
+                layer.removeChild(child);
+                layer._pMenu.setVisible(1);
+                layer._backMenu.setVisible(1);
             });
         }
     });
