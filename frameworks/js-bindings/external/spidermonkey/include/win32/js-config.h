7e86cdd5fce87a883fe0b710c05d865c6d22699d
joshuastray <joshua.astray.14@gmail.com>
2014-12-25 15:38:10 +0800

    Change win32 spidermonkey to debug build

--- frameworks/js-bindings/external/spidermonkey/include/win32/js-config.h
+++ frameworks/js-bindings/external/spidermonkey/include/win32/js-config.h
@@ -13,7 +13,7 @@
    and installed along with jsapi.h.  */
 
 /* Define to 1 if SpiderMonkey is in debug mode. */
-/* #undef JS_DEBUG */
+#define JS_DEBUG 1
 
 /* Define to 1 if SpiderMonkey should not use struct types in debug builds. */
 /* #undef JS_NO_JSVAL_JSID_STRUCT_TYPES */
@@ -26,7 +26,7 @@
 
 /* Define to 1 if SpiderMonkey should support the ability to perform
    entirely too much GC.  */
-/* #undef JS_GC_ZEAL */
+#define JS_GC_ZEAL 1
 
 /* Define to 1 if the <endian.h> header is present and
    useable.  See jscpucfg.h.  */
