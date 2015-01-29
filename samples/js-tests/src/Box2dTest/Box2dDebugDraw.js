af936b652b04378172307b880f4c57ddbc3292d3
joshuastray <joshua.astray.14@gmail.com>
2015-01-29 11:55:46 +0800

    add Box2d debugdraw and more tests

---
+++ samples/js-tests/src/Box2dTest/Box2dDebugDraw.js
@@ -0,0 +1,99 @@
+/**
+ * Created by joshua on 15-1-28.
+ */
+
+
+e_shapeBit = 0x0001;
+e_jointBit = 0x0002;
+e_aabbBit = 0x0004;
+e_pairBit = 0x0008;
+e_centerOfMassBit = 0x0010;
+
+
+cc.getBox2dDebugDraw = function(parent, PTM){
+
+    PTM = PTM || 1;
+
+    function _getColor(colorPtr, alpha){
+        var col = Box2D.wrapPointer(colorPtr, Box2D.b2Color);
+        var red = (col.get_r() * 255)|0; // |0 for asm optimization
+        var green = (col.get_g() * 255)|0;
+        var blue = (col.get_b() * 255)|0;
+
+        return cc.color(red, green, blue, alpha || 255);
+    }
+
+    function _getVerts(verticePtr, vertexCount){
+        var verts = new Array(vertexCount);
+        for(var i = 0; i < vertexCount; ++i){
+            var vertice = Box2D.wrapPointer(verticePtr + i * 8, Box2D.b2Vec2);
+            verts[i] = cc.p(vertice.get_x() * PTM, vertice.get_y() * PTM);
+        }
+
+        return verts;
+    }
+
+
+
+    var debugDraw = new Box2D.JSDraw();
+    debugDraw.drawNode = new cc.DrawNode();
+    parent.addChild(debugDraw.drawNode);
+
+    debugDraw.DrawSegment = function(vert1Ptr, vert2Ptr, colorPtr) {
+
+        var vert1 = Box2D.wrapPointer(vert1Ptr, Box2D.b2Vec2);
+        var vert2 = Box2D.wrapPointer(vert2Ptr, Box2D.b2Vec2);
+
+        var from = cc.p(vert1.get_x() * 32, vert1.get_y()* 32);
+        var to   = cc.p(vert2.get_x()* 32, vert2.get_y()* 32);
+        this.drawNode.drawSegment(from, to, 1, _getColor(colorPtr));
+    };
+
+    debugDraw.DrawPolygon = function(verticePtr, vertexCount, colorPtr){
+        this.drawNode.drawPoly(_getVerts(verticePtr, vertexCount), null, 1, _getColor(colorPtr));
+    };
+
+    debugDraw.DrawSolidPolygon = function(verticePtr, vertexCount, colorPtr) {
+        this.drawNode.drawPoly(_getVerts(verticePtr, vertexCount), _getColor(colorPtr, 150), 1, cc.color(255, 255, 255));
+    };
+
+    debugDraw.DrawCircle = function(centerPtr, radius, colorPtr) {
+        var dummyAxis = Box2D.b2Vec2(0,0);
+//        this.DrawSolidCircle(centerPtr, radius, dummyAxis, colorPtr);
+    };
+
+    debugDraw.DrawSolidCircle = function(centerPtr, radius, axisPtr, colorPtr) {
+        var centerV = Box2D.wrapPointer(centerPtr, Box2D.b2Vec2);
+        var axisV = Box2D.wrapPointer(axisPtr, Box2D.b2Vec2);
+
+        var center = cc.p(centerV.get_x() * PTM, centerV.get_y() * PTM);
+        var end = cc.p(center.x + axisV.get_x() * radius * PTM, center.y + axisV.get_y() * radius * PTM);
+
+        var color = _getColor(colorPtr);
+
+        this.drawNode.drawCircle(center, radius * PTM, 0, 50, false, 1, color);
+        this.drawNode.drawSegment(center, end, 1, color);
+    };
+
+    debugDraw.DrawTransform = function() {
+        //not implemented
+    };
+
+    debugDraw.clear = function(){this.drawNode.clear()};
+    debugDraw.SetFlags(e_shapeBit);
+    return debugDraw;
+};
+
+function createPolygonShape(vertices) {
+    var shape = new Box2D.b2PolygonShape();
+    var buffer = Box2D.allocate(vertices.length * 8, 'float', Box2D.ALLOC_STACK);
+    var offset = 0;
+    for (var i=0;i<vertices.length;i++) {
+        Box2D.setValue(buffer+(offset), vertices[i].get_x(), 'float'); // x
+        Box2D.setValue(buffer+(offset+4), vertices[i].get_y(), 'float'); // y
+        offset += 8;
+    }
+    var ptr_wrapped = Box2D.wrapPointer(buffer, Box2D.b2Vec2);
+    shape.Set(ptr_wrapped, vertices.length);
+    return shape;
+}
--- samples/js-tests/src/Box2dTest/Box2dTest.js
+++ samples/js-tests/src/Box2dTest/Box2dTest.js
@@ -26,14 +26,61 @@
 
 
 //box2d
-require('script/box2d/Box2D_v2.3.1_min.js');
+if(cc.sys.isNative)
+    require('script/box2d/Box2D_v2.3.1_min.js');
 
 var TAG_SPRITE_MANAGER = 1;
 var PTM_RATIO = 32;
 
-var Box2DTestLayer = cc.Layer.extend({
-    world:null,
-    //GLESDebugDraw *m_debugDraw;
+var DEGTORAD = 0.0174532925199432957;
+var RADTODEG = 57.295779513082320876;
+
+
+var Box2DTest = BaseTestLayer.extend({
+    debug:null,
+    ctor:function () {
+        this._super();
+        var gravity = new Box2D.b2Vec2(0.0, -10.0);
+        this.world = new Box2D.b2World(gravity);
+    },
+
+    enableDebugDraw:function(){
+        if(!this.debug){
+            this.debug = cc.getBox2dDebugDraw(this, PTM_RATIO);
+            this.world.SetDebugDraw(this.debug);
+        }
+    },
+    debugDraw:function(){
+        this.debug.clear();
+        this.world.DrawDebugData();
+    },
+
+    title:function () {
+        return "Box2D";
+    },
+
+    subtitle:function () {
+        return "";
+    },
+
+    onBackCallback:function (sender) {
+        var s = new Box2DTestScene();
+        s.addChild(previousBox2DTest());
+        director.runScene(s);
+    },
+    onRestartCallback:function (sender) {
+        var s = new Box2DTestScene();
+        s.addChild(restartBox2DTest());
+        director.runScene(s);
+    },
+    onNextCallback:function (sender) {
+        var s = new Box2DTestScene();
+        s.addChild(nextBox2DTest());
+        director.runScene(s);
+    }
+});
+
+var Box2DTest_sprite = Box2DTest.extend({
 
     ctor:function () {
 
@@ -55,9 +102,6 @@ var Box2DTestLayer = cc.Layer.extend({
 
         var screenSize = cc.director.getWinSize();
 
-        // Construct a world object, which will hold and simulate the rigid bodies.
-        var gravity = new Box2D.b2Vec2(0.0, -10.0);
-        this.world = new Box2D.b2World(gravity);
 
         var bd_ground = new Box2D.b2BodyDef();
         var ground = this.world.CreateBody(bd_ground);
@@ -73,14 +117,12 @@ var Box2DTestLayer = cc.Layer.extend({
 
         this.addNewSpriteWithCoords(cc.p(screenSize.width / 2, screenSize.height / 2));
 
-        var label = new cc.LabelTTF("Tap screen", "Marker Felt", 32);
-        this.addChild(label, 0);
-        label.color = cc.color(0, 0, 255);
-        label.x = screenSize.width / 2;
-        label.y = screenSize.height - 50;
-
         this.scheduleUpdate();
         //----end0----
+    },
+
+    subtitle: function(){
+        return "Tap screen";
     },
 
     addNewSpriteWithCoords:function (p) {
@@ -134,15 +176,340 @@ var Box2DTestLayer = cc.Layer.extend({
 
 });
 
+
+var Box2DTest_car = (function(){
+
+    var MOVE_LEFT =     0x01;
+    var MOVE_RIGHT =    0x02;
+
+    return Box2DTest.extend({
+        carBody:null,
+        rearWheelJoint:null,
+        moveFlags:0,
+        ctor:function(){
+            this._super();
+
+            var b2BodyDef = Box2D.b2BodyDef,
+                b2EdgeShape = Box2D.b2EdgeShape,
+                b2FixtureDef = Box2D.b2FixtureDef,
+                b2Vec2 = Box2D.b2Vec2,
+                b2PolygonShape = Box2D.b2PolygonShape,
+                b2RevoluteJointDef = Box2D.b2RevoluteJointDef,
+                b2CircleShape = Box2D.b2CircleShape,
+                b2WheelJointDef = Box2D.b2WheelJointDef;
+
+
+            var ground = this.world.CreateBody( new b2BodyDef() );
+
+            //ground
+            {
+                var shape = new b2EdgeShape();
+
+                var fd = new b2FixtureDef();
+                fd.set_shape(shape);
+                fd.set_density(0.0);
+                fd.set_friction(0.6);
+
+                shape.Set(new b2Vec2(-20.0, 0.0), new b2Vec2(20.0, 0.0));
+                ground.CreateFixture(fd);
+
+                //error
+                var hs = [0.25, 1.0, 4.0, 0.0, 0.0, -1.0, -2.0, -2.0, -1.25, 0.0];
+
+                x = 20.0;
+                y1 = 0.0;
+                var dx = 5.0;
+
+                for (var i = 0; i < 10; ++i)
+                {
+                    var y2 = hs[i];
+                    shape.Set( new b2Vec2(x, y1), new b2Vec2(x + dx, y2));
+                    ground.CreateFixture(fd);
+
+                    y1 = y2;
+                    x += dx;
+                }
+
+                //error
+
+                for (var i = 0; i < 10; ++i)
+                {
+                    var y2 = hs[i];
+                    shape.Set( new b2Vec2(x, y1), new b2Vec2(x + dx, y2));
+                    ground.CreateFixture(fd);
+                    y1 = y2;
+                    x += dx;
+                }
+
+                shape.Set(new b2Vec2(x, 0.0), new b2Vec2(x + 40.0, 0.0));
+                ground.CreateFixture(fd);
+
+                x += 80.0;
+                shape.Set(new b2Vec2(x, 0.0), new b2Vec2(x + 40.0, 0.0));
+                ground.CreateFixture(fd);
+
+                x += 40.0;
+                shape.Set(new b2Vec2(x, 0.0), new b2Vec2(x + 10.0, 5.0));
+                ground.CreateFixture(fd);
+
+                x += 20.0;
+                shape.Set(new b2Vec2(x, 0.0), new b2Vec2(x + 40.0, 0.0));
+                ground.CreateFixture(fd);
+
+                x += 40.0;
+                shape.Set(new b2Vec2(x, 0.0), new b2Vec2(x, 20.0));
+                ground.CreateFixture(fd);
+            }
+
+
+            // Teeter
+            {
+                var bd = new b2BodyDef();
+                bd.set_position(new b2Vec2(140.0, 1.0));
+                // bd.set_type(b2_dynamicBody);
+                bd.set_type(Box2D.b2_dynamicBody);
+                var body = this.world.CreateBody(bd);
+
+                var box = new b2PolygonShape();
+                box.SetAsBox(10.0, 0.25);
+                body.CreateFixture(box, 1);
+
+                var jd = new b2RevoluteJointDef();
+                jd.Initialize(ground, body, body.GetPosition());
+                jd.set_lowerAngle(-8 * DEGTORAD);
+                jd.set_upperAngle( 8 * DEGTORAD);
+                jd.set_enableLimit(true);
+                this.world.CreateJoint(jd);
+
+                body.ApplyAngularImpulse(100.0, true);
+            }
+
+            // Bridge
+            {
+                var N = 20;
+                shape = new b2PolygonShape();
+                shape.SetAsBox(1.0, 0.125);
+
+                var fd = new b2FixtureDef();
+                fd.set_shape( shape );
+                fd.set_density( 1.0 );
+                fd.set_friction( 0.6 );
+
+                var jd = new b2RevoluteJointDef();
+
+                var prevBody = ground;
+                var bd = new b2BodyDef();
+                // bd.set_type(b2_dynamicBody);
+                bd.set_type(Box2D.b2_dynamicBody);
+                for (var i = 0; i < N; ++i)
+                {
+                    bd.set_position( new b2Vec2(161.0 + 2.0 * i, -0.125) );
+                    var body = this.world.CreateBody(bd);
+                    body.CreateFixture(fd);
+
+                    var anchor = new b2Vec2(160.0 + 2.0 * i, -0.125);
+                    jd.Initialize(prevBody, body, anchor);
+                    this.world.CreateJoint(jd);
+
+                    prevBody = body;
+                }
+
+                var anchor = new b2Vec2(160.0 + 2.0 * N, -0.125);
+                jd.Initialize(prevBody, ground, anchor);
+                this.world.CreateJoint(jd);
+            }
+
+            // Boxes
+            {
+                var box = new b2PolygonShape();
+                box.SetAsBox(0.5, 0.5);
+
+                var bd = new b2BodyDef();
+                // bd.set_type(b2_dynamicBody);
+                bd.set_type(Box2D.b2_dynamicBody);
+
+                for (var i = 0; i < 5; i++) {
+                    bd.set_position(new b2Vec2(230.0, i + 0.5));
+                    this.world.CreateBody(bd).CreateFixture(box, 0.5);
+                }
+            }
+
+            //car
+            {
+                var carVerts = [];
+                carVerts.push( new b2Vec2(-1.5, -0.5) );
+                carVerts.push( new b2Vec2(1.5, -0.5) );
+                carVerts.push( new b2Vec2(1.5, 0.0) );
+                carVerts.push( new b2Vec2(0.0, 0.9) );
+                carVerts.push( new b2Vec2(-1.15, 0.9) );
+                carVerts.push( new b2Vec2(-1.5, 0.2) );
+                var chassisShape = new createPolygonShape(carVerts);
+
+                var circleShape = new b2CircleShape();
+                circleShape.set_m_radius(0.4);
+
+                var bd = new b2BodyDef();
+                // bd.set_type(b2_dynamicBody);
+                bd.set_type(Box2D.b2_dynamicBody);
+                bd.set_position( new b2Vec2(0,1) );
+                this.carBody = this.world.CreateBody(bd);
+                this.carBody.CreateFixture(chassisShape, 1);
+
+                var fd = new b2FixtureDef();
+                fd.set_shape(circleShape);
+                fd.set_density(1.0);
+                fd.set_friction(0.9);
+
+                bd.set_position( new b2Vec2(-1.0, 0.35) );
+                var wheelBody1 = this.world.CreateBody(bd);
+                wheelBody1.CreateFixture(fd);
+
+                bd.set_position( new b2Vec2(1.0, 0.4) );
+                var wheelBody2 = this.world.CreateBody(bd);
+                wheelBody2.CreateFixture(fd);
+
+                var m_hz = 4.0;
+                var m_zeta = 0.7;
+                var m_speed = 50.0;
+
+                var jd = new b2WheelJointDef();
+                var axis = new b2Vec2(0.0, 1.0);
+
+                jd.Initialize(this.carBody, wheelBody1, wheelBody1.GetPosition(), axis);
+                jd.set_motorSpeed(0.0);
+                jd.set_maxMotorTorque(20.0);
+                jd.set_enableMotor(true);
+                jd.set_frequencyHz(m_hz);
+                jd.set_dampingRatio(m_zeta);
+                this.rearWheelJoint = Box2D.castObject( this.world.CreateJoint(jd), b2WheelJoint );
+
+                jd.Initialize(this.carBody, wheelBody2, wheelBody2.GetPosition(), axis);
+                //jd.set_motorSpeed(0.0);
+                //jd.set_maxMotorTorque(10.0);
+                jd.set_enableMotor(false);
+                jd.set_frequencyHz(m_hz);
+                jd.set_dampingRatio(m_zeta);
+                var wheelJoint2 = Box2D.castObject( this.world.CreateJoint(jd), b2WheelJoint );
+            }
+
+            if ('keyboard' in cc.sys.capabilities) {
+                cc.eventManager.addListener({
+                    event: cc.EventListener.KEYBOARD,
+                    onKeyPressed:  function(keyCode, event){
+                        var self = event.getCurrentTarget();
+                        if ( keyCode == cc.KEY.j ) {//j
+                            self.moveFlags |= MOVE_LEFT;
+                            self.updateMotorSpeed();
+                        }
+                        else if ( keyCode == cc.KEY.k ) {//k
+                            self.moveFlags |= MOVE_RIGHT;
+                            self.updateMotorSpeed();
+                        }
+
+                    },
+                    onKeyReleased: function(keyCode, event){
+                        var self = event.getCurrentTarget();
+                        if ( keyCode == cc.KEY.j ) {//j
+                            self.moveFlags &= ~MOVE_LEFT;
+                            self.updateMotorSpeed();
+                        }
+                        else if ( keyCode == cc.KEY.k ) {//k
+                            self.moveFlags &= ~MOVE_RIGHT;
+                            self.updateMotorSpeed();
+                        }
+                    }
+                }, this);
+            }
+
+            if( 'touches' in cc.sys.capabilities ) {
+                cc.eventManager.addListener({
+                    event: cc.EventListener.TOUCH_ONE_BY_ONE,
+                    swallowTouches: true,
+                    onTouchBegan: function(touch, event){
+                        var self = event.getCurrentTarget();
+                        var pos = touch.getLocation();
+                        if( pos.x < cc.winSize.width/2) {
+                            self.moveFlags |= MOVE_LEFT;
+                            self.updateMotorSpeed();
+                        }else{
+                            self.moveFlags |= MOVE_RIGHT;
+                            self.updateMotorSpeed();
+                        }
+                        return true;
+                    },
+
+                    onTouchEnded: function(touch, event){
+                        var self = event.getCurrentTarget();
+                        var pos = touch.getLocation();
+                        if( pos.x < cc.winSize.width/2) {
+                            self.moveFlags &= ~MOVE_LEFT;
+                            self.updateMotorSpeed();
+                        }else{
+                            self.moveFlags &= ~MOVE_RIGHT;
+                            self.updateMotorSpeed();
+                        }
+                    }
+                }, this);
+            }
+
+            this.enableDebugDraw();
+            this.scheduleUpdate();
+
+            this.debug.drawNode.x = 200;
+            this.debug.drawNode.y = 100;
+        },
+
+        update:function(dt){
+            this.updateMotorSpeed();
+            //update position
+            this.debug.drawNode.x -= this.carBody.GetLinearVelocity().get_x() * dt * PTM_RATIO;
+            this.debug.drawNode.y -= this.carBody.GetLinearVelocity().get_y() * dt * PTM_RATIO;
+
+            this.world.Step(dt, 2, 2);
+            this.debugDraw();
+        },
+
+        updateMotorSpeed:function() {
+            if ( (this.moveFlags & MOVE_LEFT) == MOVE_LEFT )
+                this.rearWheelJoint.SetMotorSpeed(50);
+            else if ( (this.moveFlags & MOVE_RIGHT) == MOVE_RIGHT )
+                this.rearWheelJoint.SetMotorSpeed(-50);
+            else
+                this.rearWheelJoint.SetMotorSpeed(0);
+        }
+    });
+})();
+
+
+var Box2DTestSceneIdx = -1;
+
 var Box2DTestScene = TestScene.extend({
-    runThisTest:function () {
-        var layer = new Box2DTestLayer();
-        this.addChild(layer);
+    runThisTest:function (num) {
+        ActionMgrTestIdx = (num || 0) - 1;
+        this.addChild(nextBox2DTest());
 
         cc.director.runScene(this);
     }
 });
 
 var arrayOfBox2DTest = [
-    Box2DTestLayer
+    Box2DTest_sprite,
+    Box2DTest_car
 ];
+
+var nextBox2DTest = function () {
+    Box2DTestSceneIdx++;
+    Box2DTestSceneIdx = Box2DTestSceneIdx % arrayOfBox2DTest.length;
+
+    return new arrayOfBox2DTest[Box2DTestSceneIdx]();
+};
+var previousBox2DTest = function () {
+    Box2DTestSceneIdx--;
+    if (Box2DTestSceneIdx < 0)
+        Box2DTestSceneIdx += arrayOfBox2DTest.length;
+
+    return new arrayOfBox2DTest[Box2DTestSceneIdx]();
+};
+var restartBox2DTest = function () {
+    return new arrayOfBox2DTest[Box2DTestSceneIdx]();
+};
