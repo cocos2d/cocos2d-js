87002dcb4d27b9f0c039a3b4132dcdc3753bf6ba
jianglong0156 <jianglong0156@gmail.com>
2015-01-30 17:58:32 +0800

    replace tab to space

--- samples/js-tests/src/tests-main.js
+++ samples/js-tests/src/tests-main.js
@@ -104,7 +104,7 @@ var TestController = cc.LayerGradient.extend({
         var toggleAutoTestItem = new cc.MenuItemToggle(subItem1, subItem2);
         toggleAutoTestItem.setCallback(this.onToggleAutoTest, this);
         toggleAutoTestItem.x = winSize.width - toggleAutoTestItem.width / 2 - 10;
-	    toggleAutoTestItem.y = 20;
+        toggleAutoTestItem.y = 20;
         toggleAutoTestItem.setVisible(false);
         if( autoTestEnabled )
             toggleAutoTestItem.setSelectedIndex(1);
