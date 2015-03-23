c73e2a0fe8c886d3ecdb50d38d542c2607367e34
pandamicro <lphuabin@gmail.com>
2015-03-23 21:02:08 +0800

    Move 3D modules to jsb namespace

--- frameworks/js-bindings/bindings/script/3d/jsb_cocos2d_3d.js
+++ frameworks/js-bindings/bindings/script/3d/jsb_cocos2d_3d.js
@@ -68,7 +68,7 @@ cc.AsyncTaskPool.TaskType = {
     TASK_MAX_TYPE : 3
 };
 
-cc.BillBoard.Mode = {
+jsb.BillBoard.Mode = {
     VIEW_POINT_ORIENTED : 0, // orient to the camera
     VIEW_PLANE_ORIENTED : 1  // orient to the XOY plane of camera
 };
@@ -203,7 +203,7 @@ cc.math.vec4 = function(x, y, z, w){
     return new cc.math.Vec4(x, y, z, w);
 };
 
-cc.Sprite3D.prototype._ctor = function(modelPath, texturePath){
+jsb.Sprite3D.prototype._ctor = function(modelPath, texturePath){
     if(modelPath === undefined){
         this.init();
     }else{
@@ -220,7 +220,7 @@ cc.Sprite3D.prototype._ctor = function(modelPath, texturePath){
     }
 };
 
-cc.BillBoard.prototype._ctor = function(filename, rect, mode = cc.BillBoard.Mode.VIEW_POINT_ORIENTED){
+jsb.BillBoard.prototype._ctor = function(filename, rect, mode = cc.BillBoard.Mode.VIEW_POINT_ORIENTED){
     if(filename !== undefined && filename instanceof cc.Texture2D){
         rect = rect || cc.BillBoard.Mode.VIEW_POINT_ORIENTED;
         this.initWithTexture(filename);
