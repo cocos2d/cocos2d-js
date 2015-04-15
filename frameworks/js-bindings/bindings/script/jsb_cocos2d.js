b914198547118b95fd510e017b554ceb8f5153cf
pandamicro <lphuabin@gmail.com>
2015-04-15 15:41:46 +0800

    Add cc.BlendFunc to JSB

--- frameworks/js-bindings/bindings/script/jsb_cocos2d.js
+++ frameworks/js-bindings/bindings/script/jsb_cocos2d.js
@@ -2833,6 +2833,48 @@ _p.unscheduleAllCallbacksForTarget = _p.unscheduleAllForTarget;
 
 
 //
+// cc.BlendFunc
+//
+/**
+ * Blend Function used for textures
+ * @Class cc.BlendFunc
+ * @Constructor
+ * @param {Number} src1 source blend function
+ * @param {Number} dst1 destination blend function
+ */
+cc.BlendFunc = function (src1, dst1) {
+    this.src = src1;
+    this.dst = dst1;
+};
+
+cc.BlendFunc._disable = function(){
+    return new cc.BlendFunc(cc.ONE, cc.ZERO);
+};
+cc.BlendFunc._alphaPremultiplied = function(){
+    return new cc.BlendFunc(cc.ONE, cc.ONE_MINUS_SRC_ALPHA);
+};
+cc.BlendFunc._alphaNonPremultiplied = function(){
+    return new cc.BlendFunc(cc.SRC_ALPHA, cc.ONE_MINUS_SRC_ALPHA);
+};
+cc.BlendFunc._additive = function(){
+    return new cc.BlendFunc(cc.SRC_ALPHA, cc.ONE);
+};
+
+/** @expose */
+cc.BlendFunc.DISABLE;
+cc.defineGetterSetter(cc.BlendFunc, "DISABLE", cc.BlendFunc._disable);
+/** @expose */
+cc.BlendFunc.ALPHA_PREMULTIPLIED;
+cc.defineGetterSetter(cc.BlendFunc, "ALPHA_PREMULTIPLIED", cc.BlendFunc._alphaPremultiplied);
+/** @expose */
+cc.BlendFunc.ALPHA_NON_PREMULTIPLIED;
+cc.defineGetterSetter(cc.BlendFunc, "ALPHA_NON_PREMULTIPLIED", cc.BlendFunc._alphaNonPremultiplied);
+/** @expose */
+cc.BlendFunc.ADDITIVE;
+cc.defineGetterSetter(cc.BlendFunc, "ADDITIVE", cc.BlendFunc._additive);
+
+
+//
 // 3D module rename to jsb namespace
 //
 jsb.BaseLight = cc.BaseLight;
