42c1572f50e8efc150c561ac78f991d9c3d392cd
pandamicro <lphuabin@gmail.com>
2015-04-21 13:54:51 +0800

    Update version

--- frameworks/js-bindings/bindings/script/jsb_cocos2d.js
+++ frameworks/js-bindings/bindings/script/jsb_cocos2d.js
@@ -26,7 +26,7 @@
 
 // CCConfig.js
 //
-cc.ENGINE_VERSION = "Cocos2d-JS v3.5";
+cc.ENGINE_VERSION = "Cocos2d-JS v3.6 Beta";
 
 cc.FIX_ARTIFACTS_BY_STRECHING_TEXEL = 0;
 cc.DIRECTOR_STATS_POSITION = {x: 0, y: 0};
