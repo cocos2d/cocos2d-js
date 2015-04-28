15a7a5620c614d507263ca945de7c16e7e2447c5
pandamicro <lphuabin@gmail.com>
2015-04-28 18:29:05 +0800

    Update runtime to support Cocos Code IDE

--- templates/js-template-runtime/runtime/mac/PrebuiltRuntimeJs.app/Contents/Resources/script/studio/parsers/timelineParser-2.x.js
+++ templates/js-template-runtime/runtime/mac/PrebuiltRuntimeJs.app/Contents/Resources/script/studio/parsers/timelineParser-2.x.js
@@ -106,22 +106,18 @@
 
         setContentSize(node, json["Size"]);
 
-        if (json["Alpha"] !== undefined)
+        if (json["Alpha"] != null)
             node.setOpacity(json["Alpha"]);
 
         node.setTag(json["Tag"] || 0);
 
         var actionTag = json["ActionTag"] || 0;
-        if (cc.sys.isNative){
-            var extensionData = new ccs.ObjectExtensionData();
-            var customProperty = json["UserData"];
-            if(customProperty !== undefined)
-                extensionData.setCustomProperty(customProperty);
-            extensionData.setActionTag(actionTag);
-            node.setUserObject(extensionData);
-        } else {
-            node.setUserObject(new ccs.ActionTimelineData(actionTag));
-        }
+        var extensionData = new ccs.ObjectExtensionData();
+        var customProperty = json["UserData"];
+        if(customProperty !== undefined)
+            extensionData.setCustomProperty(customProperty);
+        extensionData.setActionTag(actionTag);
+        node.setUserObject(extensionData);
 
         node.setCascadeColorEnabled(true);
         node.setCascadeOpacityEnabled(true);
@@ -141,7 +137,7 @@
                             node.pushBackCustomItem(child);
                     } else {
                         if(!(node instanceof ccui.Layout) && child instanceof ccui.Widget) {
-                            if(child.getPositionType() == ccui.Widget.POSITION_PERCENT) {
+                            if(child.getPositionType() === ccui.Widget.POSITION_PERCENT) {
                                 var position = child.getPositionPercent();
                                 var anchor = node.getAnchorPoint();
                                 child.setPositionPercent(cc.p(position.x + anchor.x, position.y + anchor.y));
@@ -177,13 +173,23 @@
         var node =  new cc.Sprite();
 
         loadTexture(json["FileData"], resourcePath, function(path, type){
-            if(type == 0)
+            if(type === 0)
                 node.setTexture(path);
-            else if(type == 1){
+            else if(type === 1){
                 var spriteFrame = cc.spriteFrameCache.getSpriteFrame(path);
                 node.setSpriteFrame(spriteFrame);
             }
         });
+
+        var blendData = json["BlendFunc"];
+        if(json["BlendFunc"]) {
+            var blendFunc = cc.BlendFunc.ALPHA_PREMULTIPLIED;
+            if (blendData["Src"] !== undefined)
+                blendFunc.src = blendData["Src"];
+            if (blendData["Dst"] !== undefined)
+                blendFunc.dst = blendData["Dst"];
+            node.setBlendFunc(blendFunc);
+        }
 
         if(json["FlipX"])
             node.setFlippedX(true);
@@ -213,6 +219,16 @@
             node = new cc.ParticleSystem(path);
             self.generalAttributes(node, json);
             !cc.sys.isNative && node.setDrawMode(cc.ParticleSystem.TEXTURE_MODE);
+
+            var blendData = json["BlendFunc"];
+            if(json["BlendFunc"]){
+                var blendFunc = cc.BlendFunc.ALPHA_PREMULTIPLIED;
+                if(blendData["Src"] !== undefined)
+                    blendFunc.src = blendData["Src"];
+                if(blendData["Dst"] !== undefined)
+                    blendFunc.dst = blendData["Dst"];
+                node.setBlendFunc(blendFunc);
+            }
         });
         return node;
     };
@@ -222,7 +238,7 @@
     // WIDGET //
     ////////////
 
-    parser.widgetAttributes = function(widget, json) {
+    parser.widgetAttributes = function (widget, json) {
         widget.setCascadeColorEnabled(true);
         widget.setCascadeOpacityEnabled(true);
 
@@ -237,17 +253,12 @@
 
         var actionTag = json["ActionTag"] || 0;
         widget.setActionTag(actionTag);
-
-        if (cc.sys.isNative){
-            var extensionData = new ccs.ObjectExtensionData();
-            var customProperty = json["UserData"];
-            if(customProperty !== undefined)
-                extensionData.setCustomProperty(customProperty);
-            extensionData.setActionTag(actionTag);
-            widget.setUserObject(extensionData);
-        } else {
-            widget.setUserObject(new ccs.ActionTimelineData(actionTag));
-        }
+        var extensionData = new ccs.ObjectExtensionData();
+        var customProperty = json["UserData"];
+        if(customProperty !== undefined)
+            extensionData.setCustomProperty(customProperty);
+        extensionData.setActionTag(actionTag);
+        widget.setUserObject(extensionData);
 
         var rotationSkewX = json["RotationSkewX"];
         if (rotationSkewX)
@@ -358,21 +369,21 @@
         layoutComponent.setStretchHeightEnabled(stretchVerticalEnabled);
 
         var horizontalEdgeType = ccui.LayoutComponent.horizontalEdge.NONE;
-        if (horizontalEdge == "LeftEdge") {
+        if (horizontalEdge === "LeftEdge") {
             horizontalEdgeType = ccui.LayoutComponent.horizontalEdge.LEFT;
-        } else if (horizontalEdge == "RightEdge") {
+        } else if (horizontalEdge === "RightEdge") {
             horizontalEdgeType = ccui.LayoutComponent.horizontalEdge.RIGHT;
-        } else if (horizontalEdge == "BothEdge") {
+        } else if (horizontalEdge === "BothEdge") {
             horizontalEdgeType = ccui.LayoutComponent.horizontalEdge.CENTER;
         }
         layoutComponent.setHorizontalEdge(horizontalEdgeType);
 
         var verticalEdgeType = ccui.LayoutComponent.verticalEdge.NONE;
-        if (verticalEdge == "TopEdge") {
+        if (verticalEdge === "TopEdge") {
             verticalEdgeType = ccui.LayoutComponent.verticalEdge.TOP;
-        } else if (verticalEdge == "BottomEdge") {
+        } else if (verticalEdge === "BottomEdge") {
             verticalEdgeType = ccui.LayoutComponent.verticalEdge.BOTTOM;
-        } else if (verticalEdge == "BothEdge") {
+        } else if (verticalEdge === "BothEdge") {
             verticalEdgeType = ccui.LayoutComponent.verticalEdge.CENTER;
         }
         layoutComponent.setVerticalEdge(verticalEdgeType);
@@ -381,6 +392,30 @@
         layoutComponent.setBottomMargin(bottomMargin);
         layoutComponent.setLeftMargin(leftMargin);
         layoutComponent.setRightMargin(rightMargin);
+
+        layoutComponent.setVerticalEdge(verticalEdgeType);
+
+        layoutComponent.setTopMargin(topMargin);
+        layoutComponent.setBottomMargin(bottomMargin);
+        layoutComponent.setLeftMargin(leftMargin);
+        layoutComponent.setRightMargin(rightMargin);
+    };
+
+    var setLayoutBackground = function(layout, single, first, end){
+        if( layout.getBackGroundColorType() === 2 ){
+            first = first || {};
+            end = end || {};
+            layout.setBackGroundColor(getColor(first), getColor(end));
+        }else{
+            single = single || {};
+            layout.setBackGroundColor(getColor(single));
+        }
+    };
+
+    var setLayoutBackgroundVector = function(widget, vector){
+        var x = vector["ScaleX"] || 0;
+        var y = vector["ScaleY"] || 0;
+        widget.setBackGroundColorVector(cc.p(x, y));
     };
 
     /**
@@ -435,16 +470,8 @@
 
         }
 
-        var firstColor = json["FirstColor"];
-        var endColor = json["EndColor"];
-        if(endColor["R"] != null && endColor["G"] != null && endColor["B"] != null)
-            widget.setBackGroundColor(getColor(firstColor), getColor(endColor));
-        else
-            widget.setBackGroundColor(getColor(json["SingleColor"]));
-
-        var colorVector = json["ColorVector"];
-        if(colorVector != null)
-            widget.setBackGroundColorVector(cc.p(colorVector["ScaleX"], colorVector["ScaleY"]));
+        setLayoutBackground(widget, json["SingleColor"], json["FirstColor"], json["EndColor"]);
+        setLayoutBackgroundVector(widget, json["ColorVector"]);
 
         return widget;
     };
@@ -505,11 +532,6 @@
         }
         widget.setTextVerticalAlignment(v_alignment);
 
-        //todo check it
-        var isCustomSize = json["IsCustomSize"];
-        if(isCustomSize != null)
-            widget.ignoreContentAdaptWithSize(!isCustomSize);
-
         var fontResource = json["FontResource"];
         if(fontResource != null){
             var path = fontResource["Path"];
@@ -524,6 +546,20 @@
                 widget.setFontName(fontName);
             }
         }
+
+        if(json["OutlineEnabled"] && json["OutlineColor"] && widget.enableOutline)
+            widget.enableOutline(getColor(json["OutlineColor"]), getParam(json["OutlineSize"], 1));
+
+        if(json["ShadowEnabled"] && json["ShadowColor"] && widget.enableShadow)
+            widget.enableShadow(
+                getColor(json["ShadowColor"]),
+                cc.size(getParam(json["ShadowOffsetX"], 2), getParam(json["ShadowOffsetY"], -2)),
+                json["ShadowBlurRadius"] || 0
+            );
+
+        var isCustomSize = json["IsCustomSize"];
+        if(isCustomSize != null)
+            widget.ignoreContentAdaptWithSize(!isCustomSize);
 
         widget.setUnifySizeEnabled(false);
 
@@ -592,16 +628,28 @@
                 widget.setTitleFontName(fontName);
             }
         }
+
+        var label = widget.getTitleRenderer();
+        if(label && json["ShadowEnabled"] && json["ShadowColor"] && label.enableShadow){
+            label.enableShadow(
+                getColor(json["ShadowColor"]),
+                cc.size(getParam(json["ShadowOffsetX"], 2), getParam(json["ShadowOffsetY"], -2)),
+                json["ShadowBlurRadius"] || 0
+            );
+        }
+        if(label && json["OutlineEnabled"] && json["OutlineColor"] && label.enableStroke)
+            label.enableStroke(getColor(json["OutlineColor"]), getParam(json["OutlineSize"], 1));
+
         this.widgetAttributes(widget, json);
 
         if(scale9Enabled) {
             widget.setUnifySizeEnabled(false);
             widget.ignoreContentAdaptWithSize(false);
             var capInsets = cc.rect(
-                json["Scale9OriginX"] || 0,
-                json["Scale9OriginY"] || 0,
-                json["Scale9Width"] || 0,
-                json["Scale9Height"] || 0
+                    json["Scale9OriginX"] || 0,
+                    json["Scale9OriginY"] || 0,
+                    json["Scale9Width"] || 0,
+                    json["Scale9Height"] || 0
             );
             widget.setCapInsets(capInsets);
 
@@ -689,24 +737,8 @@
             setContentSize(widget, json["Size"]);
         }
 
-        var firstColor = json["FirstColor"];
-        var endColor = json["EndColor"];
-        if(firstColor && endColor){
-            if(endColor["R"] != null && endColor["G"] != null && endColor["B"] != null)
-                widget.setBackGroundColor(getColor(firstColor), getColor(endColor));
-            else
-                widget.setBackGroundColor(getColor(firstColor));
-        }else{
-            widget.setBackGroundColor(getColor(json["SingleColor"]));
-        }
-
-
-        var colorVector = json["ColorVector"];
-        if(colorVector){
-            var colorVectorX = getParam(colorVector["ScaleX"], 1);
-            var colorVectorY = getParam(colorVector["ScaleY"], 1);
-            widget.setBackGroundColorVector(cc.p(colorVectorX, colorVectorY));
-        }
+        setLayoutBackground(widget, json["SingleColor"], json["FirstColor"], json["EndColor"]);
+        setLayoutBackgroundVector(widget, json["ColorVector"]);
 
         var innerNodeSize = json["InnerNodeSize"];
         var innerSize = cc.size(
@@ -716,9 +748,9 @@
         widget.setInnerContainerSize(innerSize);
 
         var direction = 0;
-        if(json["ScrollDirectionType"] == "Vertical") direction = 1;
-        if(json["ScrollDirectionType"] == "Horizontal") direction = 2;
-        if(json["ScrollDirectionType"] == "Vertical_Horizontal") direction = 3;
+        if(json["ScrollDirectionType"] === "Vertical") direction = 1;
+        if(json["ScrollDirectionType"] === "Horizontal") direction = 2;
+        if(json["ScrollDirectionType"] === "Vertical_Horizontal") direction = 3;
         widget.setDirection(direction);
 
         var bounceEnabled = getParam(json["IsBounceEnabled"], false);
@@ -815,7 +847,7 @@
         ];
         textureList.forEach(function(item){
             loadTexture(json[item.name], resourcePath, function(path, type){
-                if(type == 0 && !loader.getRes(path))
+                if(type === 0 && !loader.getRes(path))
                     cc.log("%s need to be preloaded", path);
                 item.handle.call(widget, path, type);
             });
@@ -868,21 +900,10 @@
         var colorType = getParam(json["ComboBoxIndex"], 0);
         widget.setBackGroundColorType(colorType);
 
-        var bgColorOpacity = json["BackColorAlpha"];
-        var firstColor = json["FirstColor"];
-        var endColor = json["EndColor"];
-        if(firstColor && endColor){
-            if(endColor["R"] != null && endColor["G"] != null && endColor["B"] != null)
-                widget.setBackGroundColor(getColor(firstColor), getColor(endColor));
-            else
-                widget.setBackGroundColor(getColor(firstColor));
-        }else{
-            widget.setBackGroundColor(getColor(json["SingleColor"]));
-        }
+        setLayoutBackground(widget, json["SingleColor"], json["FirstColor"], json["EndColor"]);
+        setLayoutBackgroundVector(widget, json["ColorVector"]);
 
-        var colorVector = json["ColorVector"];
-        if(colorVector != null && colorVector["ScaleX"] != null && colorVector["ScaleY"] != null)
-            widget.setBackGroundColorVector(cc.p(colorVector["ScaleX"], colorVector["ScaleY"]));
+        var bgColorOpacity = json["BackColorAlpha"];
         if(bgColorOpacity != null)
             widget.setBackGroundColorOpacity(bgColorOpacity);
 
@@ -936,19 +957,19 @@
         var horizontalType = getParam(json["HorizontalType"], "Align_Top");
         if(!directionType){
             widget.setDirection(ccui.ScrollView.DIR_HORIZONTAL);
-            if(verticalType == "Align_Bottom")
+            if(verticalType === "Align_Bottom")
                 widget.setGravity(ccui.ListView.GRAVITY_BOTTOM);
-            else if(verticalType == "Align_VerticalCenter")
+            else if(verticalType === "Align_VerticalCenter")
                 widget.setGravity(ccui.ListView.GRAVITY_CENTER_VERTICAL);
             else
                 widget.setGravity(ccui.ListView.GRAVITY_TOP);
-        }else if(directionType == "Vertical"){
+        }else if(directionType === "Vertical"){
             widget.setDirection(ccui.ScrollView.DIR_VERTICAL);
-            if (horizontalType == "")
+            if (horizontalType === "")
                 widget.setGravity(ccui.ListView.GRAVITY_LEFT);
-            else if (horizontalType == "Align_Right")
+            else if (horizontalType === "Align_Right")
                 widget.setGravity(ccui.ListView.GRAVITY_RIGHT);
-            else if (horizontalType == "Align_HorizontalCenter")
+            else if (horizontalType === "Align_HorizontalCenter")
                 widget.setGravity(ccui.ListView.GRAVITY_CENTER_HORIZONTAL);
         }
 
@@ -964,20 +985,9 @@
         if(innerSize != null)
             widget.setInnerContainerSize(cc.size(innerSize["Widget"]||0, innerSize["Height"]||0));
 
-        var firstColor = json["FirstColor"];
-        var endColor = json["EndColor"];
-        if(firstColor && endColor){
-            if(endColor["R"] != null && endColor["G"] != null && endColor["B"] != null)
-                widget.setBackGroundColor(getColor(firstColor), getColor(endColor));
-            else
-                widget.setBackGroundColor(getColor(firstColor));
-        }else{
-            widget.setBackGroundColor(getColor(json["SingleColor"]));
-        }
+        setLayoutBackground(widget, json["SingleColor"], json["FirstColor"], json["EndColor"]);
+        setLayoutBackgroundVector(widget, json["ColorVector"]);
 
-        var colorVector = json["ColorVector"];
-        if(colorVector != null && colorVector["ScaleX"] != null && colorVector["ScaleY"] != null)
-            widget.setBackGroundColorVector(cc.p(colorVector["ScaleX"], colorVector["ScaleY"]));
         if(bgColorOpacity != null)
             widget.setBackGroundColorOpacity(bgColorOpacity);
 
@@ -1005,7 +1015,7 @@
         loadTexture(json["LabelAtlasFileImage_CNB"], resourcePath, function(path, type){
             if(!cc.loader.getRes(path))
                 cc.log("%s need to be preloaded", path);
-            if(type == 0){
+            if(type === 0){
                 widget.setProperty(stringValue, path, itemWidth, itemHeight, startCharMap);
             }
         });
@@ -1033,6 +1043,7 @@
                 cc.log("%s need to be pre loaded", path);
             widget.setFntFile(path);
         });
+        widget.ignoreContentAdaptWithSize(true);
         return widget;
     };
 
@@ -1145,7 +1156,7 @@
         var node = null;
 
         loadTexture(json["FileData"], resourcePath, function(path, type){
-            if(type == 0)
+            if(type === 0)
                 node = new cc.TMXTiledMap(path);
 
             parser.generalAttributes(node, json);
@@ -1234,7 +1245,7 @@
         if(json != null){
             var path = json["Path"];
             var type;
-            if(json["Type"] == "Default" || json["Type"] == "Normal")
+            if(json["Type"] === "Default" || json["Type"] === "Normal")
                 type = 0;
             else
                 type = 1;
@@ -1260,7 +1271,8 @@
         var r = json["R"] != null ? json["R"] : 255;
         var g = json["G"] != null ? json["G"] : 255;
         var b = json["B"] != null ? json["B"] : 255;
-        return cc.color(r, g, b);
+        var a = json["A"] != null ? json["A"] : 255;
+        return cc.color(r, g, b, a);
     };
 
     var setContentSize = function(node, size){
